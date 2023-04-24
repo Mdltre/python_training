95bf93495ec61d781b6d5f0618ac3b2f2e6be68c
mjpdelatorre <mjpdelatorre@gmail.com>
2023-04-24 11:27:21 +0800

    Created exer3
    HNGG

---
+++ exer3.py
@@ -0,0 +1,29 @@
+name1 = input("Enter Name 1: ")
+name2 = input("Enter Name 2: ")
+
+n1 = name1.replace(" ","").lower()
+n2 = name2.replace(" ","").lower()
+
+count1 = 0
+count2 = 0
+
+for i in n1:
+    if i in n2:
+        count1 += 1
+for i in n2:
+    if i in n1:
+        count2 += 1
+
+finalcount = count1 + count2
+
+def checkflmes(int):
+    res = ["FRIENDS", "LOVE", "AFFECTION", "MARRIAGE", "ENEMY", "SIBLINGS"]
+    check = int % 6 - 1
+    length = len(res)
+    for i in range(length):
+        return res[check]
+        
+
+print("Result for "+ name1 +" ==> " +str(checkflmes(count1)))
+print("Result for "+ name2 +" ==> " +str(checkflmes(count2)))
+print("FINAL RESULT ==> " + str(checkflmes(finalcount)))
