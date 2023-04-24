38602e180bdab91ed2ea1d5b2ee87fcf75bb243e
mjpdelatorre <mjpdelatorre@gmail.com>
2023-04-24 09:34:18 +0800

    Created exer1.py
    first exercise

---
+++ exer1.py
@@ -0,0 +1,16 @@
+user_input = int(input())
+
+if user_input > 59 and user_input < 75:
+    print("Derp!")
+
+elif user_input > 74 and user_input < 85:
+    print("Good")
+    
+elif user_input > 84 and user_input < 95:
+    print("Very Good")
+    
+elif user_input > 94 and user_input < 101:
+    print("Excellent!")
+    
+else: 
+    print("Invalid Value")
