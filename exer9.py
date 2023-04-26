5de34d4ee5800fc855bf155fce368b1954517feb
mjpdelatorre <mjpdelatorre@gmail.com>
2023-04-26 10:57:28 +0800

    Updates exer9
    placed the try-except inside the func

--- exer9.py
+++ exer9.py
@@ -1,5 +1,3 @@
-test = "0xff"
-
 class HexFormatError(Exception):
     "Raised if the string does not fit with the format for hexadecimal."
     pass
@@ -8,18 +6,18 @@ class MemoryError(Exception):
     "Raised if the string is beyond 4-bytes (8 hex digits)"
     pass
 
-def hexconv(string):
-    res = int(string,16)
-    print("Decimal value of inputted hex is " + str(res))
-
-try:
-    if int(test, 16):
-        if 0< len(test) <= 8:
-            hexconv(test)
+def hexconv(test):
+    try:
+        if int(test, 16):
+            if 0< len(test) <= 8:
+                res = int(test,16)
+                print("Decimal value of " + test + " is " + str(res) +".")
+            else:
+                raise MemoryError
         else:
-            raise MemoryError
-    else:
-        raise HexFormatError
+            raise HexFormatError
     
-except ValueError:
-   raise HexFormatError("Error occured: Not fit for Hexadecimal!")
+    except ValueError:
+        raise HexFormatError("Error occured: Not fit for Hexadecimal!")
+
+hexconv("0xff")
